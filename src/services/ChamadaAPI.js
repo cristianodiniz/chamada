9e29f3637aaf1d28414a58696524b362738f32e5
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-18 23:06:51 +0200

    #3 fixed bug mock data

--- src/services/ChamadaAPI.js
+++ src/services/ChamadaAPI.js
@@ -12,76 +12,75 @@ const headers = {
   Authorization: token
 };
 
-
 // export const getAllParticipants = () =>
 //   fetch(`${api}/participants`, { headers })
 //     .then(res => res.json())
 //     .then(data => data.categories);
 
-participants = [
-    {
-      image: {
-        url: "http://lorempixel.com/250/250/people?1=csd",
-        title: "Remy Sharp"
-      },
-      name: "Nina Ramos",
-      attended: {
-        sacramental: true,
-        sundaySchool: true
-      },
-      date: "2019-07-21"
+const participants = [
+  {
+    image: {
+      url: "http://lorempixel.com/250/250/people?1=csd",
+      title: "Remy Sharp"
     },
-    {
-      image: {
-        url: "http://lorempixel.com/250/250/people?1=98989",
-        title: "Remy Sharp"
-      },
-      name: "Avery Vasquez",
-      attended: {
-        sacramental: true,
-        sundaySchool: true
-      },
-      date: "2019-07-21"
+    name: "Nina Ramos",
+    attended: {
+      sacramental: true,
+      sundaySchool: true
     },
-    {
-      image: {
-        url: "http://lorempixel.com/250/250/people?1=120900y",
-        title: "Remy Sharp"
-      },
-      name: "Sergio Fernandez",
-      attended: {
-        sacramental: true,
-        sundaySchool: true
-      },
-      date: "2019-07-21"
+    date: "2019-07-21"
+  },
+  {
+    image: {
+      url: "http://lorempixel.com/250/250/people?1=98989",
+      title: "Remy Sharp"
     },
-    {
-      image: {
-        url: "http://lorempixel.com/250/250/people?1=120909",
-        title: "Remy Sharp"
-      },
-      name: "Maya Reynolds",
-      attended: {
-        sacramental: true,
-        sundaySchool: true
-      },
-      date: "2019-07-21"
+    name: "Avery Vasquez",
+    attended: {
+      sacramental: true,
+      sundaySchool: true
     },
-    {
-      image: {
-        url: "http://lorempixel.com/250/250/people?1=120909hjhkk",
-        title: "Remy Sharp"
-      },
-      name: "Imani Juarez",
-      attended: {
-        sacramental: true,
-        sundaySchool: true
-      },
-      date: "2019-07-21"
-    }
-  ]
+    date: "2019-07-21"
+  },
+  {
+    image: {
+      url: "http://lorempixel.com/250/250/people?1=120900y",
+      title: "Remy Sharp"
+    },
+    name: "Sergio Fernandez",
+    attended: {
+      sacramental: true,
+      sundaySchool: true
+    },
+    date: "2019-07-21"
+  },
+  {
+    image: {
+      url: "http://lorempixel.com/250/250/people?1=120909",
+      title: "Remy Sharp"
+    },
+    name: "Maya Reynolds",
+    attended: {
+      sacramental: true,
+      sundaySchool: true
+    },
+    date: "2019-07-21"
+  },
+  {
+    image: {
+      url: "http://lorempixel.com/250/250/people?1=120909hjhkk",
+      title: "Remy Sharp"
+    },
+    name: "Imani Juarez",
+    attended: {
+      sacramental: true,
+      sundaySchool: true
+    },
+    date: "2019-07-21"
+  }
+];
 
 export const getAllParticipants = () =>
-  new Promise((resolve, reject)=>{
-    resolve(participants)
-  })
+  new Promise((resolve, reject) => {
+    resolve(participants);
+  });
