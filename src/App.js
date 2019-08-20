40f878c715b98a7900fe6cb4b1fa4a7b8aecbbe6
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-21 00:08:58 +0200

    #3 added route for attendance and schedule

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import LoadingBar from "react-redux-loading";
 
 import AttendanceReport from "./view/attendance-report";
+import Schedules from "./view/schedules"
 import { handlerInitialData } from "./actions/shared";
 import "./App.css";
 
@@ -20,7 +21,8 @@ class App extends Component {
             <LoadingBar />
             {!loading && (
               <Fragment>
-                <Route path="/" exact component={AttendanceReport} />
+                <Route path="/schedule/:scheduleId:/attendance" exact component={AttendanceReport} />
+                <Route path="/schedule" exact component={Schedules} />
                 {/* <Route path="/:category" exact component={Dashboard} />
               <Route path="/:category/:id" component={PostPage} />  */}
               </Fragment>
