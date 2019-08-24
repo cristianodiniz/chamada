b772e815dbe87c417165f3dd68c51880afd79676
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-24 16:21:55 +0200

    #2 implement redux - Schedule search function

--- src/reducers/schedules.js
+++ src/reducers/schedules.js
@@ -1,11 +1,28 @@
-import { RECIVER_SCHEDULES } from "../actions/schedules";
+import { RECIVER_SCHEDULES, SEARCH_SCHEDULES } from "../actions/schedules";
 
-export const initialState = {}
+export const initialState = { list: [], search: "", searchResult: [] };
 
 export default function Schedules(state = initialState, action) {
   switch (action.type) {
     case RECIVER_SCHEDULES:
-      return { ...state, list: action.dates };
+      return {
+        ...state,
+        dates: action.dates,
+        search: "",
+        list: action.dates
+      };
+    case SEARCH_SCHEDULES: {
+      const { search } = action;
+      const { dates } = state;
+      const list = dates
+        ? dates.filter(it => it.date.includes(search))
+        : dates;
+      return {
+        ...state,
+        search,
+        list
+      };
+    }
     default:
       return state;
   }
