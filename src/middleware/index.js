2937375da23d2802c5f801353b8760c966f6d33b
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-24 17:55:21 +0200

    #18 implement fb - loading config to firebase

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,16 @@
 import thunk from "redux-thunk";
 import { applyMiddleware } from "redux";
 import logger from "./logger";
+import { compose } from "redux";
 
-import { getFirebase } from "react-redux-firebase";
-import { getFirestore } from "redux-firestore";
-
-export default applyMiddleware(
-  thunk.withExtraArgument({ getFirebase, getFirestore }),
-  logger
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import fdConfig from '../config/fbConfig'
+export default compose(
+  applyMiddleware(
+    thunk.withExtraArgument({ getFirebase, getFirestore }),
+    logger
+  ),
+  reduxFirestore(fdConfig),
+  reactReduxFirebase(fdConfig)
 );
