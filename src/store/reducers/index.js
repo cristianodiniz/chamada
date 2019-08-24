7afbba0e9dbc93a5387846b7491021a22c93ffed
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-24 18:44:04 +0200

    #18 adding  error reducer and actions

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,11 @@ import { combineReducers } from "redux";
 import { loadingBarReducer } from "react-redux-loading";
 import participants from "./participants";
 import schedule from "./schedules";
-
+import errors from "./errors"
 
 export default combineReducers({
   participants,
   schedule,
+  errors,
   loadingBar: loadingBarReducer
 });
