248248a06b35662c2211620c1e6e10619fe31e3f
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-25 00:26:31 +0200

    #18, #2  sending scheduleID

--- src/view/attendance-report.js
+++ src/view/attendance-report.js
@@ -1,24 +1,20 @@
-
 import React, { Component } from "react";
 
 import Box from "@material-ui/core/Box";
 
-import SearchAppBar from './search-app-bar';
-import ParticipantList from './participant-list';
+import SearchAppBar from "./search-app-bar";
+import ParticipantList from "./participant-list";
 
 class MarkAttendance extends Component {
   render() {
+    const {match} = this.props
     return (
       <Box>
         <SearchAppBar />
-      
-            <ParticipantList></ParticipantList>
-    
+        <ParticipantList scheduleId={match.params.scheduleId} />
       </Box>
     );
   }
 }
 
 export default MarkAttendance;
-
-
