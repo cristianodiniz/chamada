bfd15224fcee0f1e7d0ef519484ed67be3dba49f
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-24 18:33:55 +0200

    #18 refactoring scheduler

--- src/store/actions/schedules.js
+++ src/store/actions/schedules.js
@@ -1,37 +1,47 @@
 import { getAllSchedules } from "../../services/ChamadaAPI";
 import { showLoading, hideLoading } from "react-redux-loading";
 
-export const RECIVER_SCHEDULES = "RECIVER_SCHEDULES";
-export const SEARCH_SCHEDULES = "SEARCH_SCHEDULES";
+export const COLLECTION_NAME = "schedules";
 
-export function reciverSchedules(data) {
+// #region CREATE_SCHEDULE
+export const CREATE_SCHEDULE = "CREATE_SCHEDULE";
+export function createSchedule(schedule) {
   return {
-    type: RECIVER_SCHEDULES,
-    dates: data
+    type: CREATE_SCHEDULE,
+    schedule: schedule
   };
 }
 
+export const handleCreateSchedule = schedule => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+    firestore.getCollection(COLLECTION_NAME).add({
+      ...schedule,
+      createBy: "author name",
+      createAt: new Date()
+    }).then(()=>{
+      dispatch({
+        type: CREATE_SCHEDULE,
+        schedule: schedule
+      });
+    }).catch((error)=>{
+      dispatch({
+        type: CREATE_SCHEDULE,
+        schedule: schedule
+      });
+    })
+    
+  };
+};
+
+// #endregion CREATE_SCHEDULE
+
+// #region CREATE_SCHEDULE
+export const SEARCH_SCHEDULES = "SEARCH_SCHEDULES";
 export function searchSchedules(searchTerm) {
   return {
     type: SEARCH_SCHEDULES,
     search: searchTerm
-  };
-}
-
-export function handleReciverSchedules(callback = null) {
-  return dispatch => {
-    dispatch(showLoading());
-    getAllSchedules()
-      .then(data => {
-        dispatch(reciverSchedules(data));
-        callback && callback(null,data);
-        dispatch(hideLoading());
-      })
-      .catch(e => {
-        console.warn("Error in handleReciverSchedules: ", e);
-        callback && callback(e,null);
-        dispatch(hideLoading());
-      });
   };
 }
 
@@ -43,3 +53,33 @@ export function handleOnSearchSchedules(searchTerm) {
   };
 }
 
+// #endregion SEARCH_SCHEDULES
+
+// #region RECIVER_SCHEDULES
+export const RECIVER_SCHEDULES = "RECIVER_SCHEDULES";
+export function reciverSchedules(data) {
+  return {
+    type: RECIVER_SCHEDULES,
+    chedule: data
+  };
+}
+
+export function handleReciverSchedules(callback = null) {
+  return dispatch => {
+    dispatch(showLoading());
+    getAllSchedules()
+      .then(data => {
+        dispatch(reciverSchedules(data));
+        callback && callback(null, data);
+        dispatch(hideLoading());
+      })
+      .catch(e => {
+        console.warn("Error in handleReciverSchedules: ", e);
+        callback && callback(e, null);
+        dispatch(hideLoading());
+      });
+  };
+}
+
+
+// #endregion RECIVER_SCHEDULES
