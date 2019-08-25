6800d5dc558663ecccf47a3769bf0e0031f7a0b7
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-25 03:11:56 +0200

    #19 create auth object in the store

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -5,10 +5,12 @@ import { firestoreReducer } from "redux-firestore";
 import participants from "./participants";
 import schedule from "./schedules";
 import errors from "./errors";
+import authReducer from './authReducer'
 
 export default combineReducers({
   participants,
   schedule,
+  auth: authReducer,
   errors,
   loadingBar: loadingBarReducer,
   firestore : firestoreReducer
