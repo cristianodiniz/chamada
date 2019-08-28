e24d5026beef120ea5af3ff5df3760eac843b233
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-29 00:56:46 +0200

    #26 implemented seach participant

--- src/store/reducers/participants.js
+++ src/store/reducers/participants.js
@@ -1,11 +1,16 @@
-import { RECIVER_PARTICIPANTS } from "../actions/participants";
+import { RECIVER_PARTICIPANTS, SEARCH_PERSON } from "../actions/participants";
 
-export const initialState = {}
+export const initialState = {list:[],search:""};
 
 export default function participants(state = initialState, action) {
   switch (action.type) {
     case RECIVER_PARTICIPANTS:
       return { ...state, list: action.participants };
+    case SEARCH_PERSON:
+      return {
+        ...state,
+        search: action.search
+      };
     default:
       return state;
   }
