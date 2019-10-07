1078bd7e8c738bc5ca85ab920edf13dccdd99e4b
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-10-08 00:23:16 +0200

    #51 creating ui AddOrEditSchedule

--- src/view/schedules/add-or-edit-schedule.js
+++ src/view/schedules/add-or-edit-schedule.js
@@ -3,47 +3,79 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-// import DateFnsUtils from '@date-io/date-fns';
-import { KeyboardDatePicker } from "@material-ui/pickers";
+import Button from "@material-ui/core/Button";
 
 import AppToolBar from "../common/app-tool-bar";
 import { handleOnSearchSchedules } from "../../store/actions/schedules";
 
-class SchedulesView extends Component {
+class AddOrEditSchedule extends Component {
+  state = {
+    date: new Date(),
+    description: ""
+  };
+
+  handleChangeDate = e => {
+    const value = e.target.value;
+    this.setState({ date: value });
+  };
+
+  handleChangeDescription = e => {
+    const value = e.target.value;
+    this.setState({ description: value });
+  };
+
+  onSaveSchedule = ()=>{
+    
+  }
+
   render() {
+    const { classes } = this.props;
+    // const
+
     return (
       <Box>
         <AppToolBar />
-        <form>
-          <KeyboardDatePicker
-            disableToolbar
-            variant="inline"
-            format="MM/dd/yyyy"
-            margin="normal"
-            id="date-picker-inline"
-            label="Date picker inline"
-            // value={selectedDate}
-            // onChange={handleDateChange}
-            KeyboardButtonProps={{
-              "aria-label": "change date"
-            }}
-          />
-          <TextField
-            id="filled-name"
-            label="Name"
-            // className={classes.textField}
-            // value={values.name}
-            // onChange={handleChange('name')}
-            margin="normal"
-            variant="filled"
-          />
-        </form>
+        <Container className={classes.content}>
+          {/* <form className={classes.content}> */}
+            <TextField
+              id="filled-name"
+              label="Name"
+              type="date"
+              dataDateFormat="DD MMMM YYYY"
+              // className={classes.textField}
+              value={this.state.date}
+              onChange={this.handleChangeDate}
+              margin="normal"
+              variant="filled"
+            />
+            {/* <TextField
+              id="filled-name"
+              label="Name"
+              // className={classes.textField}
+              value={this.state.description}
+              onChange={this.handleChangeDescription}
+              margin="normal"
+              variant="filled"
+            /> */}
+            <Button variant="contained" color="primary" className={classes.button} onClick={this.onSaveSchedule}>
+              Save
+            </Button>
+          {/* </form> */}
+        </Container>
       </Box>
     );
   }
 }
+
+const styles = {
+  content: {
+    display: "flex",
+    flexDirection: "column"
+  }
+};
 
 function mapStateToProps({ schedule = {} }, props) {
   return {
@@ -57,4 +89,4 @@ const mapDispatchToProps = dispatch =>
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SchedulesView);
+)(withStyles(styles)(AddOrEditSchedule));
