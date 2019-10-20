ada95de56c28851e9d10fea291c99f4449c42a57
Cristiano Diniz Pinto <cristianopinto@DinizMacPro.local>
2019-10-20 21:20:48 +0200

    fixed bug getFirebase

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,12 @@
 import thunk from "redux-thunk";
 import { applyMiddleware, compose } from "redux";
-import { getFirebase } from "react-redux-firebase";
+// import { getFirebase } from "react-redux-firebase";
 import { getFirestore, reduxFirestore } from "redux-firestore";
 
 import logger from "./logger";
 import firebase from "../config/fbConfig";
+
+const getFirebase = () => firebase
 
 export default compose(
   applyMiddleware(
