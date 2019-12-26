869a325225b3955c10bfcf41b7e09a08fd70db46
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-12-26 22:19:56 +0100

    wraping SearchAppBar component in renderWithRedux

--- src/view/common/app-tool-bar.test.js
+++ src/view/common/app-tool-bar.test.js
@@ -2,17 +2,18 @@ import React from "react";
 import SearchAppBar from "./app-tool-bar";
 import renderer from "react-test-renderer";
 import ReactDOM from "react-dom";
+import {renderWithRedux} from '../../config/reduxConfig'
 
 describe("App Tool Bar Tests Component", function() {
   it("Should render without crashing", async done => {
     const div = document.createElement("div");
-    ReactDOM.render(<SearchAppBar />, div);
+    ReactDOM.render(renderWithRedux(<SearchAppBar />), div);
     ReactDOM.unmountComponentAtNode(div);
     done();
   }, 10000);
 
   test("Should Match Snapshot", async done => {
-    const component = renderer.create(<SearchAppBar />);
+    const component = renderer.create(renderWithRedux(<SearchAppBar />));
 
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
