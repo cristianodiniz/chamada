b9aafa12ccc4b0bb49f5f809fbcfafdf3cb8c2d8
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-12-27 22:56:00 +0100

    created organization schema validatator and implemented it on createOrganization

---
+++ src/schemas/index.js
@@ -0,0 +1,39 @@
+import Validator from "schema-validator";
+import { RegexPatterns } from "../utils/utils";
+
+export const validateOrganization = organization => {
+    const schema = {
+        name: {
+            type: String,
+            required: true,
+            length: {
+                min: 4,
+                max: 20
+            },
+            test:  /^[a-zA-Z0-9]+$/gi
+        },
+
+        email: {
+            type: String,
+            required: true,
+            length: {
+                min: 4,
+                max: 20
+            },
+            test: RegexPatterns.Email.regex
+        },
+        password: {
+            type: String,
+            required: true,
+            length: {
+                min: 4,
+                max: 20
+            },
+            test: RegexPatterns.Password.regex
+        }
+    };
+
+    const validator = new Validator(schema);
+    validator.debug = true;
+    return validator.check(organization);
+};
--- src/services/organizationServices.js
+++ src/services/organizationServices.js
@@ -1,13 +1,12 @@
-export const createOrganization = ({name,email,password}) => {
-    if (!name || !email || !password)
-        throw new Error("The propers name, email and password muss be informed");
+import { validateOrganization } from "../schemas";
 
-    if (name.length < 4){
-        throw new Error("The organization name should be greater then 4 and lesser then 20");
-    }
-
-    if (name.length > 20){
-        throw new Error("The organization name should be greater then 4 and lesser then 20");
-    }
-    // return true
+export const createOrganization = (organization) => {
+    return new Promise((resolve, reject) => {
+        const validate = validateOrganization(organization);
+        if (validate._error) {
+            reject(validate);
+        } else {
+            resolve();
+        }
+    });
 };
